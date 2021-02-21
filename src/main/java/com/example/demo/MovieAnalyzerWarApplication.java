c8bcffd12b327b2b4914ea25cc70ac6763f471de
Mathias <math8802@stud.kea.dk>
2021-02-21 14:34:48 +0100

    sut mig

---
+++ src/main/java/com/example/demo/MovieAnalyzerWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAnalyzerWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAnalyzerWarApplication.class, args);
+    }
+
+}
